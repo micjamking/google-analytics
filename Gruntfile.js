04cb0ea05e90bc16cc4704a33e8a2d2bd1943828
Mike King <mike@ikayzo.com>
2015-01-21 18:32:44 -1000

    Updated Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -378,7 +378,21 @@ module.exports = function (grunt) {
         'imagemin',
         'svgmin'
       ]
-    }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:micjamking/google-analytics.git',
+          branch: 'gh-pages',
+        }
+      }                                                                                   }
   });
 
 
@@ -441,4 +455,6 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+
+  grunt.registerTask('deploy', ['buildcontrol']);
 };
